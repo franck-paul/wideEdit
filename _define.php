f7d5045015ef8e93c115d41adb7673aab3d86004
franck <carnet.franck.paul@gmail.com>
2013-09-17 13:19:22 +0200

    2.6 admin compliance

--- _define.php
+++ _define.php
@@ -1,12 +1,12 @@
 <?php
 # -- BEGIN LICENSE BLOCK ----------------------------------
 # This file is part of wideEdit, a plugin for Dotclear 2.
-# 
+#
 # Copyright (c) Franck Paul and Alain Vagner
 # carnet.franck.paul@gmail.com
 #
 # Icon from Faenza set by tiheum (http://tiheum.deviantart.com/art/Faenza-Icons-173323228)
-# 
+#
 # Licensed under the GPL version 2.0 license.
 # A copy of this license is available in LICENSE file or at
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
