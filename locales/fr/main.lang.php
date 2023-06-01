3660865a41a3c98613527ae6db0d23205430165a
franck <carnet.franck.paul@gmail.com>
2023-06-01 15:51:23 +0200

    l10n

--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -12,6 +12,6 @@
 use Dotclear\Helper\L10n;
 
 L10n::$locales['Collapse/Expand post\'s attributes sidebar'] = 'Masque/Affiche la colonne d\'options du billet en cours d\'dition';
-L10n::$locales['wideEdit'] = '';
+L10n::$locales['wideEdit'] = 'wideEdit';
 L10n::$locales['Show Options'] = 'Afficher les options';
 L10n::$locales['Hide Options'] = 'Cacher les options';
