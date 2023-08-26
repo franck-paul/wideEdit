049d4668e6c60e23b88e707299f093c0f9dc7d07
franck <carnet.franck.paul@gmail.com>
2023-08-26 12:49:14 +0200

    New release 3.0

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     'wideEdit',
     'Collapse/Expand posts attributes sidebar',
     'Franck Paul & Alain Vagner',
-    '2.1.1',
+    '3.0',
     [
         'requires'    => [['core', '2.24']],
         'permissions' => dcCore::app()->auth->makePermissions([
