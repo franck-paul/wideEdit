b6bb1412898cb77e719fc23d1a9069b0abd6355c
franck <carnet.franck.paul@gmail.com>
2023-10-25 12:16:05 +0200

    New release 4.0

--- _define.php
+++ _define.php
@@ -17,7 +17,7 @@ $this->registerModule(
     'wideEdit',
     'Collapse/Expand posts attributes sidebar',
     'Franck Paul & Alain Vagner',
-    '3.0',
+    '4.0',
     [
         'requires'    => [['core', '2.28']],
         'permissions' => 'My',
