44939522db5c4b2fcc73655a120e904b37a5691b
franck <carnet.franck.paul@gmail.com>
2024-12-26 16:05:50 +0100

    PHP Code review (Rector/PHPStan)

--- src/Backend.php
+++ src/Backend.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @brief wideEdit, a plugin for Dotclear 2
  *
@@ -22,7 +23,8 @@ class Backend extends Process
     public static function init(): bool
     {
         // dead but useful code, in order to have translations
-        __('wideEdit') . __('Collapse/Expand post\'s attributes sidebar');
+        __('wideEdit');
+        __('Collapse/Expand post\'s attributes sidebar');
 
         return self::status(My::checkContext(My::BACKEND));
     }
